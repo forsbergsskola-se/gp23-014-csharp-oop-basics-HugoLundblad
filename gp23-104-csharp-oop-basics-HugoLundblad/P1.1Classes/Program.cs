2b6f871ea78b99302aa73eba39ee889de0af4a2c
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-10-09 16:18:13 +0200

    P 1.1 Classes

---
+++ gp23-104-csharp-oop-basics-HugoLundblad/P1.1Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Person person = new Person();
+Animal animal = new Animal();
+Car car = new Car();
+
+Console.WriteLine(person);
+Console.WriteLine(animal);
+Console.WriteLine(car);
+
+public class Person {
+    
+}
+
+public class Animal {
+    
+}
+
+public class Car {
+    
+}
+
