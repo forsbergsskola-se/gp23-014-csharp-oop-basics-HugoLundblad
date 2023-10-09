7cd5a95e6ca439ea6a81a37320305054af367281
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-10-09 16:18:49 +0200

    P 2.1 Player Gold

---
+++ gp23-104-csharp-oop-basics-HugoLundblad/P2.1PlayerGold/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Player player = new Player();
+player.Gold = 200;
+
+Console.WriteLine(player.Gold);
+
+public class Player {
+    public int Gold;
+}
