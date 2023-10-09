e9f226c4d40001a328663ee977171e400b5d072b
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-10-09 16:18:31 +0200

    P 1 People

---
+++ gp23-104-csharp-oop-basics-HugoLundblad/P1People/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Person[] people = new Person[3];
+
+for (int i = 0; i < people.Length; i++)
+{
+    people[i] = new Person();
+}
+
+for (int i = 0; i < people.Length; i++)
+{
+    Console.WriteLine(people[i]);
+}
+
+public class Person {
+    
+}
