59ef87be4af148c983bdd0d4199708fcba6e38ff
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-10-10 16:11:53 +0200

    P 2.2 Item Values

---
+++ gp23-104-csharp-oop-basics-HugoLundblad/P2.2ItemValues/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Item[] items = new Item[3];
+
+for (int i = 0; i < items.Length; i++)
+{
+    items[i] = new Item();
+    items[i].goldValue += 100 + (100 * i);
+}
+
+for (int i = 0; i < items.Length; i++)
+{
+    Console.WriteLine(items[i].goldValue);
+}
+
+public class Item {
+    public int goldValue;
+}
