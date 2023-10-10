32aca0922b369791a42b9a39a573e5148c444867
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-10-10 16:11:34 +0200

    P 2 Fields

---
+++ gp23-104-csharp-oop-basics-HugoLundblad/P2Fields/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Player playerA = new Player();
+
+playerA.Gold = 200;
+playerA.Stone = 110;
+playerA.Wood = 53;
+playerA.Level = 12;
+playerA.Experience = 123;
+playerA.Subscribed = true;
+playerA.PvPToggle = false;
+playerA.PlayerName = "Marc";
+playerA.PlayerMail = "marc@zaku.de";
+
+Console.WriteLine($"Player A has {playerA.Gold} Gold, {playerA.Stone} Stone, {playerA.Wood} Wood. He is level {playerA.Level} and has {playerA.Experience} Experience.");
+Console.WriteLine("He " + (playerA.Subscribed ? "has" : "does not have") + " an active VIP subscription and PVP is " + (playerA.PvPToggle ? "enabled." : "disabled."));
+Console.WriteLine($"His display name is {playerA.PlayerName} and his e-mail address is {playerA.PlayerMail}.");
+
+public class Player {
+    public int Gold;
+    public int Stone;
+    public int Wood;
+    public int Level;
+    public int Experience;
+    public bool Subscribed;
+    public bool PvPToggle;
+    public string PlayerName;
+    public string PlayerMail;
+}
