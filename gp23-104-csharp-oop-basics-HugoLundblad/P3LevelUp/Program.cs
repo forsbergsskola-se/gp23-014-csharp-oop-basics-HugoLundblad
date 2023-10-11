c24232f9ef3cef1cf96e46fc50d36b1bd077a724
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-10-11 16:02:33 +0200

    P 3 Level Up

---
+++ gp23-104-csharp-oop-basics-HugoLundblad/P3LevelUp/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    Player p1 = new Player();
+    Console.WriteLine($"Lvl: {p1.Level}, Exp: {p1.Experience}");
+    p1.GrantExperience();
+}
+
+public class Player {
+    public int Level;
+    public int Experience;
+    
+    public void GrantExperience() {
+        Experience += int.Parse(Console.ReadLine());
+        
+        // Level Up
+        if(Experience >= 100)
+        {
+            Level += Experience / 100;
+            Experience %= 100;
+        }
+        
+        Console.WriteLine($"Lvl: {Level}, Exp: {Experience}");
+    }
+}
